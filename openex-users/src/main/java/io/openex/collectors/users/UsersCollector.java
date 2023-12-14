5807604825635afd19d89520326752cf67a75e33
Romuald Lemesle <romuald.lemesle@filigran.io>
2023-12-14 16:37:15 +0100

    [devex] Change namespace javax to jakarta

--- openex-users/src/main/java/io/openex/collectors/users/UsersCollector.java
+++ openex-users/src/main/java/io/openex/collectors/users/UsersCollector.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.time.Duration;
 
 @Component
